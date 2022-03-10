89a89c206f5930495c02410c30e0bacf279373fd
KushKloud <85106450+KushKloud@users.noreply.github.com>
2022-03-11 01:18:27 +0300

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -21,7 +21,6 @@ class button():
         self.text = text
 
     def draw(self,win,outline=None):
-        #Call this method to draw the button on the screen
         if outline:
             pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
             
@@ -33,7 +32,6 @@ class button():
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def isOver(self, pos):
-        #Pos is the mouse position or a tuple of (x,y) coordinates
         if pos[0] > self.x and pos[0] < self.x + self.width:
             if pos[1] > self.y and pos[1] < self.y + self.height:
                 return True
@@ -52,7 +50,6 @@ def redrawWindow():
 
 run = True
 
-#This takes all the info from __init__
 Button1 = button((0,255,0), 125, 15, 250, 100, 'Letter Password')
 Button2 = button((0,255,0), 125, 150, 250, 100, 'Numbers Password')
 Button3 = button((0,255,0), 125, 285, 250, 100, 'Letters and Numbers Password')
